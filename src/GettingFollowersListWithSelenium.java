50196863bf91419655d6efeeff2098550f745bae
Vahid Aghayev <aghayevvahid1@gmail.com>
2023-08-01 15:36:37 +0400

    Refactoring

--- src/GettingFollowersListWithSelenium.java
+++ src/GettingFollowersListWithSelenium.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -21,14 +20,14 @@ public class GettingFollowersListWithSelenium {
 
         driver.manage().window().maximize();
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement usernameInput =driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input"));
         WebElement passwordInput =driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input"));
         WebElement submitBtn =driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button"));
 
         usernameInput.sendKeys("4gayev1");
-        passwordInput.sendKeys("menim19yasimvar");
+        passwordInput.sendKeys("menim21yasimvar");
         submitBtn.click();
 
         Thread.sleep(3000);
@@ -52,30 +51,39 @@ public class GettingFollowersListWithSelenium {
         WebElement followersBtn = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a"));
         followersBtn.click();
 
+        Thread.sleep(3000);
         WebElement scrollbar = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"));
 
 
         for (int i = 0; i <10 ; i++) {
             jsExecutor.executeScript("arguments[0].scrollTop += arguments[0].scrollHeight;", scrollbar);
+            Thread.sleep(2000);
         }
 
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
 
-        List<WebElement> ListofFollowers = driver.findElements(By.cssSelector(".x9f619.xjbqb8w.x1rg5ohu.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x6s0dn4.x1oa3qoh.x1nhvcw1"));
+        List<WebElement> ListofFollowers = driver.findElements(By.cssSelector("._aacl._aaco._aacw._aacx._aad7._aade"));
         List<String> names = new ArrayList<>();
 
+
         for(WebElement item : ListofFollowers) {
+            System.out.println(item.getText());
             names.add(item.getText());
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/Agayev/Desktop/1/followers1.txt"))) {
-            writer.write(String.valueOf(names));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/Agayev/Desktop/1/followers2.txt"))) {
+            for(String item :names) {
+                writer.write(String.valueOf(item));
+                writer.newLine();
+            }
             System.out.println("File written successfully.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
     }
 }
